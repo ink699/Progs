3f2f84891b0663d8c772b06a07874f7784c41dbf
Ilya Kochergin <ink699@mail.ru>
2017-09-16 01:42:39 +0300

    Hometask of seminar 2 was added

---
+++ Sem2/0_fork_status.c
@@ -0,0 +1,22 @@
+/* Read the "4_exec_dir.c" carefully and correct the mistakes. Read about fork's return codes. */
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+
+int main() {
+	pid_t pid;
+  int status, died;
+  switch(pid = fork())
+	{
+  	case 0:
+			execl("/bin/ls","ls","-l",NULL); // this is the code the child runs
+			break;
+		case -1:
+			printf("err");
+			break;
+  	default:
+			wait(&status); // this is the code the parent runs
+   	}
+	return 0;
+}
---
+++ Sem2/1_cmd_runner.c
@@ -0,0 +1,82 @@
+/*
+  Write a program which allow you to:
+- run another programs via command line.
+- get exit codes of terminated programs
+
+## TIPS:
+1. Use "2_fork_wait_exit.c" and "4_exec_dir.c" from examples. Combine them.
+2. Parse input string according to the type of exec* (see "man exec").
+   a) if execvp is used, string splitting into "path" and "args" is all you need.
+3. Collect exit codes via waitpid/WEXITSTATUS.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+  pid_t pid = 1;
+  int i = 0, j = 0;
+  char c;
+  int ret_code = 0;
+  int args_num = 0;
+  char ret = ' ';
+  char adr[100];
+  char name[100];
+  char* arg[100];
+  for(i = 0; i < 100; ++i)
+  {
+    arg[i] = (char*)malloc(100*sizeof(char)) ;
+  }
+  char t;
+  while(tolower(ret) != 'n')
+  {
+    switch(pid)
+    {
+      case 0:
+        execvp(adr, arg);
+        return 0;
+      default:
+        i = 0;
+        j = 0;
+        printf("Enter address, enter quit for exit\n");
+        scanf("%s", adr);
+        if(strcmp(adr, "quit") == 0)
+          return 0;
+        args_num = argc;
+        printf("Enter arguments (including name) without line breaks\n");
+        c = getchar();
+        c = getchar();
+        for(j = 0; c != '\n'; ++j)
+        {
+          if(c == ' ')
+          {
+            *(arg[i] + j) = '\0';
+            ++i;
+            j = 0;
+          }
+          else
+          {
+            *(arg[i] + j) = c;
+          }
+          c = getchar();
+        }
+        arg[i + 1] = NULL;
+        pid = fork();
+        if(pid == -1)
+        {
+          printf("err");
+          return 0;
+        }
+        if(pid != 0)
+        {
+          wait(&ret_code);
+          printf("Return code: %d\n", WEXITSTATUS(ret_code));
+        }
+      }
+  }
+}
